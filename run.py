a81a1b37394802ef0ce3fbb768083f516f1dea96
Lucas Wilson <lkwilson96@gmail.com>
2022-05-26 19:47:40 -0600

    update rate

--- run.py
+++ run.py
@@ -4,14 +4,14 @@ import struct
 import threading
 import sys
 
-def multi_listener(running_cb, multicast_group: str, multicast_port: int, msg: bytes, length: int):
+def multi_listener(running_cb, multicast_group: str, multicast_port: int, msg: bytes, sleep: float, length: int):
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_IP)
   sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
   sock.bind((multicast_group, multicast_port))
   mreq = struct.pack("4sl", socket.inet_aton(multicast_group), socket.INADDR_ANY)
 
   sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-  sock.settimeout(1)
+  sock.settimeout(sleep)
 
   # a quick google search says 508 bytes is the smallest fragmentation size of
   # something. that's good enough for broadcasting ip addresses.
@@ -24,7 +24,7 @@ def multi_listener(running_cb, multicast_group: str, multicast_port: int, msg: b
       continue
     print('data:', data.decode('utf-8'))
 
-def multi_sender(running_cb, multicast_group: str, multicast_port: int, multicast_ttl: int, msg: bytes, sleep: int):
+def multi_sender(running_cb, multicast_group: str, multicast_port: int, multicast_ttl: int, msg: bytes, sleep: float):
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_IP)
   sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, multicast_ttl)
   while running_cb():
@@ -36,7 +36,7 @@ def main():
   multicast_port = 5007
   multicast_ttl = 1
   running = True
-  sleep = 1
+  sleep = 2
   length = 508
   msg = sys.argv[1].encode('utf-8')[:length]
 
@@ -44,7 +44,7 @@ def main():
     return running
 
   send_thread = threading.Thread(target=multi_sender, args=(running_cb, multicast_group, multicast_port, multicast_ttl, msg, sleep))
-  listen_thread = threading.Thread(target=multi_listener, args=(running_cb, multicast_group, multicast_port, msg, length))
+  listen_thread = threading.Thread(target=multi_listener, args=(running_cb, multicast_group, multicast_port, msg, sleep, length))
 
   try:
     send_thread.start()
